db148bb69f5d00ec4b136d4ba18cf6816ee9a875
Phil Rukin <philipp@rukin.me>
2020-09-03 13:17:52 +0200

    Fix links for old dribbble and behance posts

--- main.py
+++ main.py
@@ -44,13 +44,15 @@ def story_redirect(short_id):
 def behance_redirect(short_id):
   """Redirect to behance project url"""
   try:
-    project_id = str(shortener.decode(short_id))
+    project_id = shortener.decode(short_id)
   except:
     return abort(400)
   redirect_url = memcache.get('b/{}'.format(project_id))
   if not redirect_url:
+    logging.info('Not in cache: {}'.format(project_id))
     project = ndb.Key(BehanceProject, project_id).get()
     if not project:
+      logging.info('Not in DB: {}'.format(project_id))
       return make_response('<h1>Service Unavailable</h1><p>Try again later</p>', 503, {'Retry-After': 5})
     project.add_memcache()
     redirect_url = project.url
@@ -70,13 +72,15 @@ def comments_redirect(short_id):
 def dribbble_redirect(short_id):
   """Redirect to dribbble shot url"""
   try:
-    shot_id = str(shortener.decode(short_id))
+    shot_id = shortener.decode(short_id)
   except:
     return abort(400)
   redirect_url = memcache.get('d/{}'.format(shot_id))
   if not redirect_url:
+    logging.info('Not in cache: {}'.format(shot_id))
     shot = ndb.Key(DribbbleShot, shot_id).get()
     if not shot:
+      logging.info('Not in DB: {}'.format(shot_id))
       return make_response('<h1>Service Unavailable</h1><p>Try again later</p>', 503, {'Retry-After': 5})
     shot.add_memcache()
     redirect_url = shot.url
